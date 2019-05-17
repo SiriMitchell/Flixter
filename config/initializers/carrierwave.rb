ee5eaa572ba138c7d62bea2f4eb830c1b5667191
Siri Mitchell <mitchellschoolofexcellence@gmail.com>
2019-05-17 21:03:45 +0000

    slight problem w heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,12 @@
 
 
-CarrierWave.configure do |config|
-  config.storage = 'aws'
-  config.aws_bucket = ENV.fetch('S3_SIRINOMSTERBUCKET')
-  config.aws_credentials = {
-    access_key_id:       ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key:   ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    region:              ENV.fetch('AWS_REGION'),
-    stub_responses:      Rails.env.test?
-  }
+#CarrierWave.configure do |config|
+#  config.fog_credentials = {
+#    :provider => 'AWS'
+#    :aws_access_key_id => ENV["AWS_ACCESS_KEY"],
+#    :aws_secret_access_key => ENV["AWS_SECRET_KEY"],
+#    :region => ENV["S3_REGION"]
+#  }
      
-  config.aws_acl   = 'private'
-end
+#  config.fog_directory = ENV["AWS_BUCKET"]
+#end
